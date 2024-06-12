5e678dcc222bbfed4d47c2f409b548c89a73a685
balaji231001073 <231001073@rajalakshmi.edu.in>
2024-06-12 13:21:36 +0530

    Week 4-09.py

---
+++ Week-04/4-09.py
@@ -0,0 +1,59 @@
+Given a positive integer N, check whether it can be represented as a product of single digit numbers.
+
+
+
+Input Format:
+
+Single Integer input.
+
+  
+Output Format:
+
+Output displays Yes if condition satisfies else prints No.
+
+  
+Input:
+
+14
+
+
+Output:
+
+Yes
+
+  
+Input:
+
+13
+
+  
+Output:
+
+No
+
+
+
+  
+Coding:
+
+a=int(input())
+
+flag=0
+
+for i in range(10):
+
+    for j in range(10):
+
+        if(i*j==a):
+
+            flag=1
+
+            break
+
+if(flag==1):
+
+    print("Yes")
+
+else:
+
+    print("No")
