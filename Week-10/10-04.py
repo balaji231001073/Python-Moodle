b40fcf85a84016f072055cb2e32f463f7851a1a2
balaji231001073 <231001073@rajalakshmi.edu.in>
2024-06-13 12:05:06 +0530

    Create 10-04.py

---
+++ Week-10/10-04.py
@@ -0,0 +1,25 @@
+Write a Python program for binary search.
+
+
+
+For example:
+
+Input              	Result
+
+1 2 3 5 8           False
+
+6
+
+3 5 9 45 42         True
+
+42
+
+
+
+Coding:
+
+a = input().split(",")
+
+b = input()
+
+print(b in a)
